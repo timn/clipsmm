3d6c1428a5f0f03ba8b5e8212a803331ab2dbb2d
Rick L. Vinyard, Jr <rvinyard@cs.nmsu.edu>
2006-07-28 03:34:43 +0000

    Added support for finding boost smart pointer in lieu of tr1 and optionally building unit tests with cppunit.
    git-svn-id: https://clipsmm.svn.sourceforge.net/svnroot/clipsmm/trunk@19 6274f2d8-75c9-4245-8663-d2d1d42643b4

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 set -e
 
-# $Id: autogen.sh,v 1.2 2006-07-10 04:03:19 rvinyard Exp $
+# $Id: autogen.sh,v 1.3 2006-07-28 03:34:43 rvinyard Exp $
 #
 # Copyright (c) 2002  Daniel Elstner  <daniel.elstner@gmx.net>
 #
@@ -50,4 +50,4 @@ do_cmd $LIBTOOLIZE -c --force
 do_cmd $AUTOHEADER
 do_cmd $AUTOCONF
 do_cmd $AUTOMAKE -c --add-missing
-do_cmd ./configure --enable-doxygen-doc "$@"
+do_cmd ./configure --enable-doxygen-doc --enable-unit-tests "$@"
