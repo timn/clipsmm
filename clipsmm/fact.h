d93d93b3bec0df2c4a11b424dc62c3fb8d7812e0
Rick L. Vinyard, Jr <rvinyard@cs.nmsu.edu>
2009-07-29 15:58:42 +0000

    git-svn-id: https://clipsmm.svn.sourceforge.net/svnroot/clipsmm/trunk@40 6274f2d8-75c9-4245-8663-d2d1d42643b4

--- clipsmm/fact.h
+++ clipsmm/fact.h
@@ -1,21 +1,20 @@
 /***************************************************************************
- *   clipsmm C++ wrapper for the CLIPS c library                           *
  *   Copyright (C) 2006 by Rick L. Vinyard, Jr.                            *
  *   rvinyard@cs.nmsu.edu                                                  *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of version 2 of the GNU General Public License as  *
- *   published by the Free Software Foundation.                            *
+ *   This file is part of the clipsmm library.                             *
  *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
+ *   The clipsmm library is free software; you can redistribute it and/or  *
+ *   modify it under the terms of the GNU General Public License           *
+ *   version 3 as published by the Free Software Foundation.               *
  *                                                                         *
- *   You should have received a copy of the GNU Lesser General Public      *
- *   License along with this library; if not, write to the                 *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA              *
+ *   The clipsmm library is distributed in the hope that it will be        *
+ *   useful, but WITHOUT ANY WARRANTY; without even the implied warranty   *
+ *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU   *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this software. If not see <http://www.gnu.org/licenses/>.  *
  ***************************************************************************/
 #ifndef CLIPSFACT_H
 #define CLIPSFACT_H
--- clipsmm/pointer.h
+++ clipsmm/pointer.h
@@ -1,21 +1,20 @@
 /***************************************************************************
- *   clipsmm C++ wrapper for the CLIPS c library                           *
- *   Copyright (C) 2006 by Rick L. Vinyard, Jr.                            *
+ *   Copyright (C) 2006,2009 by Rick L. Vinyard, Jr.                       *
  *   rvinyard@cs.nmsu.edu                                                  *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of version 2 of the GNU General Public License as  *
- *   published by the Free Software Foundation.                            *
+ *   This file is part of the clipsmm library.                             *
  *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
+ *   The clipsmm library is free software; you can redistribute it and/or  *
+ *   modify it under the terms of the GNU General Public License           *
+ *   version 3 as published by the Free Software Foundation.               *
  *                                                                         *
- *   You should have received a copy of the GNU Lesser General Public      *
- *   License along with this library; if not, write to the                 *
- *   Free Software Foundation, Inc.,                                       *
- *   51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA              *
+ *   The clipsmm library is distributed in the hope that it will be        *
+ *   useful, but WITHOUT ANY WARRANTY; without even the implied warranty   *
+ *   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU   *
+ *   General Public License for more details.                              *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this software. If not see <http://www.gnu.org/licenses/>.  *
  ***************************************************************************/
 #include <clipsmm/clipsmm-config.h>
 
@@ -24,14 +23,33 @@
   // Headers for smart pointers
   #ifdef CLIPSMM_USE_BOOST_SMART_POINTER
     #include <boost/shared_ptr.hpp>
+    //#include <boost/weak_ptr.hpp>
     #define CLIPSPointer boost::shared_ptr
+    #define CLIPSWeakPointer boost::weak_ptr
+    #define clips_static_pointer_cast  boost::static_pointer_cast
+    #define clips_const_pointer_cast   boost::const_pointer_cast
+    #define clips_dynamic_pointer_cast boost::dynamic_pointer_cast
   #else
-    #include <typeinfo>
-    #include <memory>
-    #include <functional>
-    #include <bits/concurrence.h>
-    #include <ext/mt_allocator.h>
-    #include <tr1/boost_shared_ptr.h>
-    #define CLIPSPointer std::tr1::shared_ptr
+    #ifdef CLIPSMM_USE_CXX0X_SMART_POINTER
+      #include <memory>
+      #define CLIPSPointer std::shared_ptr
+      #define CLIPSWeakPointer std::weak_ptr
+      #define clips_static_pointer_cast  std::static_pointer_cast
+      #define clips_const_pointer_cast   std::const_pointer_cast
+      #define clips_dynamic_pointer_cast std::dynamic_pointer_cast
+    #else
+      #include <typeinfo>
+      #include <memory>
+      #include <functional>
+      #include <bits/concurrence.h>
+      #include <ext/mt_allocator.h>
+      #include <tr1/boost_shared_ptr.h>
+      #define CLIPSPointer std::tr1::shared_ptr
+      #define CLIPSWeakPointer std::tr1::weak_ptr
+      #define clips_static_pointer_cast  std::tr1::static_pointer_cast
+      #define clips_const_pointer_cast   std::tr1::const_pointer_cast
+      #define clips_dynamic_pointer_cast std::tr1::dynamic_pointer_cast
+    #endif
   #endif
+
 #endif
